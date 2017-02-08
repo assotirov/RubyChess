7b1d9e63745e2710f3dc14253b4b78edabab073f
assotirov <assotirov@uni-sofia.bg>
2017-02-08 16:49:42 +0200

    Update Bishop.rb

--- lib/Bishop.rb
+++ lib/Bishop.rb
@@ -1,11 +1,11 @@
-load "Piece.rb"
+load 'Piece.rb'
 
 class Bishop < Piece
   attr_reader :loc
 
   def initialize(board, color, loc = nil)
     super(board, color, loc)
-    @name = "Bishop"
+    @name = 'Bishop'
     @loc = loc
     if color == :black
       @sym = :b
